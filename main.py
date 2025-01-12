01e8b6f97185b0fdc78b21eb8a16c7e6bcf24237
Sean <sean.koon12@gmail.com>
2025-01-13 04:27:15 +1100

    add counter for each char in text

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
-  print(count_words(text))
+  word_count = get_number_of_words(text)
+  print(f"{word_count} words found in the text")
+  
+  print("Number of each character found in the text")
+  char_dict = get_number_of_each_char(text)
+  print(char_dict)
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
   
-def count_words(text):
+def get_number_of_words(text):
   words = text.split()
   return len(words)
+
+def get_number_of_each_char(text):
+  lowered_text = text.lower()
+  char_dict = {}
+  for char in lowered_text:
+    if char in char_dict:
+      char_dict[char] += 1
+    else:
+      char_dict[char] = 1
+  return char_dict
 
 main()
